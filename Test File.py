0335f396cd94a2835f4d1b90c4a4da0809f08627
Capitan Nemo <aliayati752@gmail.com>
2018-05-05 11:50:37 +0430

    BUGS Fixed
    waiting for DataBase !

--- Test File.py
+++ Test File.py
@@ -2,12 +2,14 @@
 # Developer --> Ali78_CE
 
 # import libraries
-import urllib2
+import urllib2, os, time
 from bs4 import BeautifulSoup
 # Specify the URL
 global MyURL
 MyURL = 'https://cafebazaar.ir'
-URL = "https://cafebazaar.ir/lists/ml-best-new-action-games/?l=en"
+PAGE = 1
+URL = "https://cafebazaar.ir/lists/ml-best-new-action-games/?l=en&p=%s"%str(PAGE)
+Application = []
 
 def get_first_info(URL):
 	global MyURL
@@ -25,25 +27,25 @@ def get_first_info(URL):
 	names = SOUP.findAll('div', attrs={'class':'msht-app-name'})
 	for name in names:
 		name = name.text.strip()
-		app_names.append(name)
+		app_names.append(str(name))
 
 	# Take out app ratings
 	ratings = SOUP.findAll('div', attrs={'class':'rating-fill'})
 	for rate in ratings:
 		rate = rate.get('style').replace('width:', '')
-		app_ratings.append(rate)
+		app_ratings.append(str(rate))
 
 	# Take out app prices
 	prices = SOUP.findAll('div', attrs={'class':'msht-app-price'})
 	for price in prices:
 		price = price.text.strip()
-		app_prices.append(price)
+		app_prices.append(str(price))
 
 	# Take out app urls
 	urls = SOUP.findAll('a', attrs={'itemprop':'url'})
 	for url in urls:
 		url = MyURL + url.get('href').strip()
-		app_links.append(url)
+		app_links.append(str(url))
 
 	# Take out app images
 	images = SOUP.findAll('img')
@@ -51,16 +53,16 @@ def get_first_info(URL):
 		img = img.get('src').strip()
 		img = img[2:]
 		if '128x128' in img:
-			app_images_src.append(img)
+			app_images_src.append(str(img))
 	return app_names, app_prices, app_links, app_ratings, app_images_src
 
 
 def print_(list_):
 	for i in list_:
 		print i
+		print '\n'
 
-"""
-app_names, app_prices, app_links, app_ratings, app_images_src = get_first_info(URL)
+'''
 print ('app_names')
 print_(app_names)
 print ('app_prices')
@@ -71,11 +73,12 @@ print ('app_ratings')
 print_(app_ratings)
 print ('app_images_src')
 print_(app_images_src)
-"""
-APPURL = 'https://cafebazaar.ir/app/com.pomelogames.TowerGame/?l=en'
+'''
+APPURL = 'https://cafebazaar.ir/app/com.craneballs.flingfighters/?l=en'
 
 def get_second_info(AppUrl):
 	DetailsList = []
+	TestList = []
 	AppDetails = {}
 	# Query the website and return the HTML to the variable HTML
 	HTML = urllib2.urlopen(AppUrl)
@@ -83,15 +86,36 @@ def get_second_info(AppUrl):
 	SOUP = BeautifulSoup(HTML, 'html.parser')
 
 	details = SOUP.findAll('span', attrs={'class':'pull-right'})
-	for detail in details:
-			DetailsList.append(str(detail.text.strip()))
-	DetailsList = DetailsList[len(DetailsList)-4:]
 
-	AppDetails['Category'] = DetailsList[0]
-	AppDetails['Active Installs'] = DetailsList[1]
-	AppDetails['Size'] = DetailsList[2]
-	AppDetails['Version'] = DetailsList[3]
+	for detail in details:
+		element = str(detail.text.strip())
+		DetailsList.append(element)
+	
+	for element in range(len(DetailsList)):
+		if '+' in DetailsList[element]:
+			TestList.append(DetailsList[element-1])
+			TestList.append(DetailsList[element])
+			TestList.append(DetailsList[element+1])
+			TestList.append(DetailsList[element+2])
+			break
+
+	AppDetails['category'] = TestList[0]
+	AppDetails['active_installs'] = TestList[1]
+	AppDetails['size'] = TestList[2]
+	AppDetails['version'] = TestList[3]
 	
 	return AppDetails
 
-print(get_second_info(APPURL))
+#print(get_second_info(APPURL))
+
+app_names, app_prices, app_links, app_ratings, app_images_src = get_first_info(URL)
+#print_(app_links)
+print '[Page :%s] loading... \n'%str(PAGE)
+for i in range(24):
+	print 'geting Application [%s] data'%i
+	dic1 = {'app_name':app_names[i], 'app_price':app_prices[i],'app_rating':app_ratings[i], 'app_image_src':app_images_src[i], 'app_link':app_links[i]}
+	dic2 = get_second_info(app_links[i])
+	dic1.update(dic2)
+	Application.append(dic1)
+os.system('cls')
+print_(Application)
